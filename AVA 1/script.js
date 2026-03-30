162b719d109f20c6588a1b536843657ad2a8aa33
Hell-duardo <eduardo.barros95@hotmail.com>
2026-03-29 22:14:41 -0300

    Add files via upload

---
+++ AVA 1/script.js
@@ -0,0 +1,7 @@
+// script.js
+const toggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+toggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
